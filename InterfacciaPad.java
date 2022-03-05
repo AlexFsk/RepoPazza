8593b9cff7b252e040744d9902c78f81dbcbd762
DEFEO ALEX <ALEX.DEFEO@cz.lan>
2022-03-05 08:54:11 +0100

    updated various file

---
+++ InterfacciaPad.java
@@ -0,0 +1,10 @@
+public interface InterfacciaPad{
+    public abstract void cerchio();
+    public abstract void quadrato();
+    public abstract void triangolo();
+    public abstract void ics();
+    public abstract void su();
+    public abstract void giu();
+    public abstract void destra();
+    public abstract void sinistra();
+}
